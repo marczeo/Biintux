c6f37153e030e21852cfd85a90f15a72407ab7e6
Marco Gómez <marco.gomez@outlook.com>
2016-12-27 20:08:09 -0600

    updata register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => $data['password'],
+            'password' => bcrypt($data['password']),
             'role_id' => $data['role_id'],
         ]);
     }

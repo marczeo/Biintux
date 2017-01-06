b83fb6681fc73b7ae01af2e47078b5ba3aa941af
R.C. Anaya <c7x117@hotmail.com>
2017-01-05 19:47:56 -0600

    Adiciones a seccion de idiomas
    Se implemento cambio de idioma en la seccion de recuperacion de
    contrase;a

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,13 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password'       => 'Passwords must be at least six characters and match the confirmation.',
+    'reset'          => 'Your password has been reset!',
+    'sent'           => 'We have e-mailed your password reset link!',
+    'token'          => 'This password reset token is invalid.',
+    'user'           => "We can't find a user with that e-mail address.",
+    'email_label'    => 'E-Mail Address',
+    'reset_title'    => 'Reset Password',
+    'Btn_reset_link' => 'Send Password Reset Link',
 
 ];

8c8e8c21b125dd98babebe0c1a5a17e2350ca0be
Marco Gómez <marco.gomez@outlook.com>
2017-02-23 12:39:44 -0600

    INIT jwt-auth
    Usar jwt-auth para usar token cuando se hagan peticiones desde la app
    https://github.com/tymondesigns/jwt-auth/wiki/Installation

--- config/app.php
+++ config/app.php
@@ -182,6 +182,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /* 
+         *token 
+         *https://github.com/tymondesigns/jwt-auth/wiki/Installation
+         */
+        'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
 
     ],
 
@@ -230,6 +236,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+        'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory',
 
     ],
 

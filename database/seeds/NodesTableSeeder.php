ee110300faaa15b19e8a6ddddb965a88b60a915b
R.C. Anaya <c7x117@hotmail.com>
2017-03-15 20:08:47 -0600

    Node Seeder

--- database/seeds/NodesTableSeeder.php
+++ database/seeds/NodesTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Filesystem\FileNotFoundException;
+use App\Repositories\MibiciRepository;
+use App\Node;
 class NodesTableSeeder extends Seeder
 {
     /**
@@ -11,6 +13,11 @@ class NodesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $content = File::get('mibici_stations');
+
+        foreach ($content as $line)
+        {
+   	 		//echo htmlspecialchars($line) ."<br />\n";
+		}
     }
 }

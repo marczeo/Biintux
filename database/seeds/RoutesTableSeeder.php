0d40703aecf6a12cec6d20eb84f4f7a9ac82868a
Marco Gómez <marco.gomez@outlook.com>
2017-03-16 14:09:24 -0600

    add routes seeder

--- database/seeds/RoutesTableSeeder.php
+++ database/seeds/RoutesTableSeeder.php
@@ -12,18 +12,42 @@ class RoutesTableSeeder extends Seeder
      */
     public function run()
     {
-        $DAO = new RouteRepository;
-        $newRoute= new Route;
-        $newRoute->setColor();
-        while ($DAO->existColor($newRoute->color)){
+        $directory = database_path("/rutas");
+        $directory_decode = database_path("/decode");
+        $files = File::allFiles($directory);
+        foreach ($files as $file)
+        {
+            $json = File::get((string)$file);
+            $obj=json_decode($json);
+            
+            $DAO = new RouteRepository;
+            $newRoute= new Route;
+            $newRoute2=new Route;
+
             $newRoute->setColor();
+            while ($DAO->existColor($newRoute->color)){
+                $newRoute->setColor();
+            }
+
+            $decode1=File::get($directory_decode."/".$obj->id."_decodedPath.txt");
+            $newRoute->code=$obj->name;
+            $newRoute->encodepath=$decode1;
+            $newRoute->direction=1;
+            $newRoute->first_run="5:00";
+            $newRoute->last_run="22:00";
+            $newRoute->type="bus";
+            $newRoute->save();
+
+            $decode2=File::get($directory_decode."/".$obj->id."_decodedPath2.txt");
+            $newRoute2->code=$newRoute->code;
+            $newRoute2->encodepath=$decode2;
+            $newRoute2->direction=2;
+            $newRoute2->first_run="5:00";
+            $newRoute2->last_run="22:00";
+            $newRoute2->type="bus";
+            $newRoute2->color=$newRoute->color;
+            $newRoute2->save();
+            
         }
-        $newRoute->code="Test";
-        $newRoute->encodepath="test";
-        $newRoute->direction=1;
-        $newRoute->first_run="5:00";
-        $newRoute->last_run="22:00";
-        $newRoute->type="bus";
-        $newRoute->save();
     }
 }
